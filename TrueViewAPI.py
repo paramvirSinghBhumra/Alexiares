8acbcc7b3812409e15901314c3b707aefdd5e196
Paramvir Singh Bhumra <43279222+paramvirSinghBhumra@users.noreply.github.com>
2021-04-23 23:45:29 -0600

    heroku implementation

--- TrueViewAPI.py
+++ TrueViewAPI.py
@@ -1,5 +1,5 @@
 #!flask/bin/python
-from HANA_Backend import get_columnNames, True_View
+from FlaskAPI.HANA_Backend import get_columnNames, True_View
 
 from flask import Flask, jsonify
 from flask import make_response
